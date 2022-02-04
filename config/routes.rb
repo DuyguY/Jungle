52a95bd9d68c48a37d107a4b6e64b8b630ffb175
DuyguY <duygu.yldrm4820@gmail.com>
2022-02-04 11:10:47 -0500

    add about features

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -12,9 +11,19 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:show]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
